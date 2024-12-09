3c95d4cf220f079ed49c2ce81f8952ace95ae935
pavan-prog <pavan.vangala215@gmail.com>
2024-12-09 15:38:47 +0530

    2.second commit

--- src/sb/for_git/GitDemo.java
+++ src/sb/for_git/GitDemo.java
@@ -1,9 +1,14 @@
 package sb.for_git;
 
+import sb.for_git.ex_1.Student;
+
 public class GitDemo {
 	public static void main(String[] args) {
 		
+		Student student = new Student(1,"pavan");
+		
 		System.out.println("This project is to learn Git");
+		System.out.println("hlell");
 	}
 
 }
---
+++ src/sb/for_git/ex_1/Student.java
@@ -0,0 +1,17 @@
+package sb.for_git.ex_1;
+
+public class Student {
+
+	private int id;
+	private String name;
+	
+	
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+	
+}
